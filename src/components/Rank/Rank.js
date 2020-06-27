5566f4cb5e3f82825ba1417a0625bf6ab3e4bc67
jatin0814 <201951073@gmail.com>
2020-06-27 16:20:59 +0530

    adding some end points

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Rank = () =>{
-	return(	
-		<div>
-			<div className='white f3'>
-				{'Jatin, your rank is...'}
-			</div>
-		<div className='white f3'>
-			{'#4'}
-		</div>
-		<div>
-			{'This Brain will Detect faces in your pictures.'}
-		</div>
-		</div>
-	);
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
+      </div>
+    </div>
+  );
 }
-export default Rank; 
+
+export default Rank;
